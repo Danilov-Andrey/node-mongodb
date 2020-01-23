200f4535c70e181785d1a90311982b162d244106
Danilov-Andrey <andydanilov98@gmail.com>
2020-01-23 23:29:23 +0400

    init commit

---
+++ routes/tourRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const tourController = require('../controllers/tourController')
+const router = express.Router()
+
+router.param('id', tourController.checkId)
+
+router
+    .route('/')
+    .get(tourController.getTours)
+    .post(tourController.checkBody, tourController.postTour)
+
+router
+    .route('/:id')
+    .get(tourController.getTour)
+    .patch(tourController.patchTour)
+    .delete(tourController.deleteTour)
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv')
+
+const app = require('./app')
+
+dotenv.config({ path: './config.env' })
+
+app.listen(process.env.PORT, () => {
+    console.log('Start express')
+})
